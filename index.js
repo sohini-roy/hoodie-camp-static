5e51d685a251798689a5b20e67f459ceafff4b3c
sohini-roy <sohinirc45@gmail.com>
2017-03-22 18:05:24 +0530

    implement /get_issues

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ var http = require('http')
 var app = express();
 
 var options = {
-  host: 'www.api.github.com'
-  path: 'repo/hoodiehq/camp/issues'
+  host: 'api.github.com',
+  path: '/repos/hoodiehq/camp/issues',
+  method: 'GET',
+  headers: 'headers: {'user-agent':'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'}'
 }
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -15,7 +18,37 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/get_issues', function (req, res) {
-  res.send('bleh')
+  var request = https.request(options, function(response){
+    var body = '';
+    response.on('data',function(chunk){
+        body+=chunk;
+    });
+
+    response.on('end',function(){
+        var issues_data = JSON.parse(body);
+        var response_data = {
+          camp_issues: issues_data.length,
+          first_pr: 0,
+          first_timer: 0
+        }
+        for(var i=0;i<issues_data.length;i++){
+          for(var j=0;j<issues_data[i].label.length;j++){
+            if(issues_data[i].label[j].name=="Your First PR"){
+              response_data.frist_pr++
+            }
+            if(issues_data[i].label[j].name=="first-timers-only"){
+              response_data.frist_timer++
+            }
+          }
+        }
+        res.send(response_data)
+    });
+
+    request.on('error', function(e) {
+      console.error('Error: '+e);
+    });
+    request.end();
+  });
 })
 
 app.use(express.static('public'));
